6afae4af9ab6ec63bfd980ef3eccb23fff3ddb6e
Julio Cesar <jccintr@gmail.com>
2025-01-25 19:18:28 -0300

    Implementado CommandLineRunner

--- src/main/java/com/jcsoftware/jspdesafio0/Jspdesafio0Application.java
+++ src/main/java/com/jcsoftware/jspdesafio0/Jspdesafio0Application.java
@@ -1,13 +1,21 @@
 package com.jcsoftware.jspdesafio0;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Jspdesafio0Application {
+public class Jspdesafio0Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Jspdesafio0Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("Testando !");
+		
 	}
 
 }
